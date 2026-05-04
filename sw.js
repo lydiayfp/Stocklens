417e25684eda2bf3adb662e30e6ee8358e26f6b1
lydiayfp <lydiayfp@gmail.com>
2026-05-04 16:04:54 +0800

    Add grep command to sw.js for metric extraction

---
+++ sw.js
@@ -0,0 +1,1 @@
+grep -n "function extractMetrics\|function buildFund\|RC(\|TR(\|renderIV\|ivG\|ivGN\|ivD\|ivF\b\|M\.pe\|M\.pb\|M\.roe\|M\.fcf\|M\.peg\|M\.eps\b" /home/claude/StockLens_v8/index.html | head -60
