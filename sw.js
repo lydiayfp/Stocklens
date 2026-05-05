0cc9d301ff99395ee23bd496a22b88d9d8e87f81
lydiayfp <lydiayfp@gmail.com>
2026-05-05 08:40:20 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-grep -n "function extractMetrics\|function buildFund\|RC(\|TR(\|renderIV\|ivG\|ivGN\|ivD\|ivF\b\|M\.pe\|M\.pb\|M\.roe\|M\.fcf\|M\.peg\|M\.eps\b" /home/claude/StockLens_v8/index.html | head -60
